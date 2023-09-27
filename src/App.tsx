f829630a149e45f836a8aad95be2caeb51539f2c
vinod1104 <81498161+vinod1104@users.noreply.github.com>
2023-09-27 11:56:48 +0530

    Update App.tsx
    1.Corrected wrong values of median and mode in all classes.
    2. Re written efficient code.

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,22 @@ const App = () => {
   //To calculate the class-wise mean, median, mode of “Flavanoids” for the entire dataset.
 
   const flavanoidsData = (totalData: any[], alcoholNum: number) => {
-    //   console.log(totalData)
     const filterData = totalData.filter((item: { Alcohol: number; }) => item.Alcohol === alcoholNum);
-    const meanFlad = filterData.map(item => parseFloat(item.Flavanoids));
+    const fladData = filterData.map(item => parseFloat(item.Flavanoids));
 
     // Calculate mean
-    const mean = meanFlad.reduce((sum, value) => sum + value, 0) / meanFlad.length;
+    const mean = fladData.reduce((sum, value) => sum + value, 0) / fladData.length;
 
     // Calculate median
-    filterData.sort((a: any, b: any) => a.Flavanoids - b.Flavanoids);
-    const middle = Math.floor(filterData.length / 2);
-    const median =
-      filterData.length % 2 === 0 ? filterData[Math.round((middle + (middle - 1)) / 2)] : filterData[middle];
-
+    fladData.sort((a: any, b: any) => a.Flavanoids - b.Flavanoids);
+    const middle = Math.floor(fladData.length / 2);
+    const median = fladData.length % 2 === 0 ? (fladData[middle - 1] + fladData[middle]) / 2 : fladData[middle];
     // Calculate mode
     const counts: any = {};
     let mode = null;
     let maxCount = 0;
-    filterData.forEach((value: string | number) => {
+
+    fladData.forEach((value: any | number) => {
       counts[value] = (counts[value] || 0) + 1;
       if (counts[value] > maxCount) {
         mode = value;
@@ -37,28 +35,29 @@ const App = () => {
   //create a new property “Gamma” for each point of the dataset. “Gamma” can be calculated as Gamma = (Ash * Hue) / Magnesium.  Thereafter, calculate the class-wise mean, median, mode of “Gamma” for the entire dataset
 
   const gammaData = (p_gammaData: any[], p_gNum: number) => {
-    p_gammaData.map((eachItem: any) => {
+    const filterData = p_gammaData.filter((item: { Alcohol: number; }) => item.Alcohol === p_gNum);
+    filterData.map((eachItem: any) => {
       let Gamma: number
-      Gamma = (parseFloat(eachItem.Ash) * parseFloat(eachItem.Hue)) / parseFloat(eachItem.Magnesium)
+      Gamma = (parseFloat(eachItem.Ash) * eachItem.Hue) / eachItem.Magnesium
       eachItem["Gamma"] = Gamma
     })
-    const filterData = p_gammaData.filter((item: { Alcohol: number; }) => item.Alcohol === p_gNum);
-    const meanFlad = filterData.map(item => parseFloat(item.Gamma));
+    const gammaFlad = filterData.map(item => item.Gamma);
 
     // Calculate mean
-    const mean = meanFlad.reduce((sum, value) => sum + value, 0) / meanFlad.length;
+    const mean = gammaFlad.reduce((sum, value) => sum + value, 0) / gammaFlad.length;
 
     // Calculate median
-    filterData.sort((a: any, b: any) => a.Gamma - b.Gamma);
+    gammaFlad.sort((a: any, b: any) => a.Gamma - b.Gamma);
     const middle = Math.floor(filterData.length / 2);
     const median =
-      filterData.length % 2 === 0 ? filterData[Math.round((middle + (middle - 1)) / 2)] : filterData[middle];
+      gammaFlad.length % 2 === 0 ? (gammaFlad[middle - 1] + gammaFlad[middle]) / 2
+        : gammaFlad[middle];
 
     // Calculate mode
     const counts: any = {};
     let mode = null;
     let maxCount = 0;
-    filterData.forEach((value: string | number) => {
+    gammaFlad.forEach((value: string | number) => {
       counts[value] = (counts[value] || 0) + 1;
       if (counts[value] > maxCount) {
         mode = value;
@@ -93,15 +92,15 @@ const App = () => {
           </tr>
           <tr>
             <td>Flavanoids Median</td>
-            <td>{flavanoids1.median.Flavanoids.toFixed(3)}</td>
-            <td>{flavanoids2.median.Flavanoids.toFixed(3)}</td>
-            <td>{flavanoids3.median.Flavanoids.toFixed(3)}</td>
+            <td>{flavanoids1.median.toFixed(3)}</td>
+            <td>{flavanoids2.median.toFixed(3)}</td>
+            <td>{flavanoids3.median.toFixed(3)}</td>
           </tr>
           <tr>
             <td>Flavanoids Mode</td>
-            <td>{flavanoids1.mode.Flavanoids.toFixed(3)}</td>
-            <td>{flavanoids2.mode.Flavanoids.toFixed(3)}</td>
-            <td>{flavanoids3.mode.Flavanoids.toFixed(3)}</td>
+            <td>{flavanoids1.mode.toFixed(3)}</td>
+            <td>{flavanoids2.mode.toFixed(3)}</td>
+            <td>{flavanoids3.mode.toFixed(3)}</td>
           </tr>
 
         </tbody>
@@ -118,25 +117,22 @@ const App = () => {
           </thead>
           <tbody>
             <tr>
-              <td>Gamma
-                Mean</td>
+              <td>Gamma Mean</td>
               <td>{gammaStats1.mean.toFixed(3)}</td>
               <td>{gammaStats2.mean.toFixed(3)}</td>
               <td>{gammaStats3.mean.toFixed(3)}</td>
             </tr>
             <tr>
-              <td>Gamma
-                Median</td>
-              <td>{gammaStats1.median.Gamma.toFixed(3)}</td>
-              <td>{gammaStats2.median.Gamma.toFixed(3)}</td>
-              <td>{gammaStats3.median.Gamma.toFixed(3)}</td>
+              <td>Gamma Median</td>
+              <td>{gammaStats1.median.toFixed(3)}</td>
+              <td>{gammaStats2.median.toFixed(3)}</td>
+              <td>{gammaStats3.median.toFixed(3)}</td>
             </tr>
             <tr>
-              <td>Gamma
-                Mode</td>
-              <td>{gammaStats1.mode.Gamma.toFixed(3)}</td>
-              <td>{gammaStats2.mode.Gamma.toFixed(3)}</td>
-              <td>{gammaStats3.mode.Gamma.toFixed(3)}</td>
+              <td>Gamma Mode</td>
+              <td>{gammaStats1.mode.toFixed(3)}</td>
+              <td>{gammaStats2.mode.toFixed(3)}</td>
+              <td>{gammaStats3.mode.toFixed(3)}</td>
             </tr>
 
           </tbody>
